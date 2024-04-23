8de5d7f476f772203da9f1cf34619c050763e449
tuan3110 <chodai23590@gmail.com>
2024-04-23 13:54:12 +0700

    update test page

--- client-web/pages/Test.jsx
+++ client-web/pages/Test.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 export default function Test() {
+  const loader = useLoaderData();
+
+  // Kiểm tra xem loader có dữ liệu không
+  if (!loader || !loader.data || !loader.data.categories) {
+    return <div>Loading...</div>;
+  }
+
+  const { categories } = loader.data;
+
   return (
-    <div>Test</div>
-  )
+    <div>
+      <h1>Danh sách các loại dịch vụ</h1>
+      <ul>
+        {categories.map(category => (
+          <li key={category.id}>
+            {category.name} - Type: {category.type || 'Unknown'}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
--- client-web/src/router/index.jsx
+++ client-web/src/router/index.jsx
@@ -21,7 +21,29 @@ export default createBrowserRouter([
     },
     {
         element: <Test/>,
-        path: '/test'
+        path: '/test',
+        loader: async ()=>{
+            const query = `query MyQuery {
+                categories {
+                  name
+                  id
+                  type
+                }
+              }
+              `;
+            const res = await fetch('https://api-ap-southeast-2.hygraph.com/v2/clv4uoiq108fp07w7579676h9/master',{
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                },
+                body: JSON.stringify({
+                    query
+                })
+            });
+            const data = await res.json();
+            return data;
+        }
     }]
 }
 ])
