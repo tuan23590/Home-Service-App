d8b41e8d4effb0f2a67d412a749718d7980fde6d
tuan3110 <chodai23590@gmail.com>
2024-05-31 09:16:24 +0700

    chore: Update ThoiLuong component to display khoiLuongCongViec instead of moTaDichVu

--- client-web/src/components/ThemDonHang.jsx
+++ client-web/src/components/ThemDonHang.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { TextField, Button, Grid, Typography, Paper, Select, MenuItem, FormControl, InputLabel, Autocomplete, Table, TableHead, TableRow, TableCell, TableBody, Checkbox } from '@mui/material';
 import { apiQuanHuyen, apiTinhTP, apiXaPhuong } from '../../utils/DiaChiUtil';
-import { dichVuLoader,apiDanhSachDichVuChinh } from '../../utils/DichVuUtils';
+import { dichVuLoader, apiDanhSachDichVuChinh } from '../../utils/DichVuUtils';
 
 const ThemDonHang = () => {
     const [errors, setErrors] = useState({});
@@ -11,6 +11,8 @@ const ThemDonHang = () => {
         khachHang: '',
         dichVuChinh: null,
         danhSachDichVuThem: [''],
+        ngayBatDau: null,
+        soThangLapLai: null,
         vatNuoi: '',
         ghiChu: '',
         uuTienTasker: '',
@@ -36,7 +38,58 @@ const ThemDonHang = () => {
     const [danhSachTinhTp, setDanhSachTinhTp] = useState([]);
     const [danhSachQuanHuyen, setDanhSachQuanHuyen] = useState([]);
     const [danhSachXaPhuong, setDanhSachXaPhuong] = useState([]);
+    const [danhSachNgayThucHien, setDanhSachNgayThucHien] = useState([]);
+    const [danhSachGioThucHien, setDanhSachGioThucHien] = useState([]);
 
+
+    useEffect(() => {
+        const daysOfWeek = ["Chủ Nhật", "Thứ Hai", "Thứ Ba", "Thứ Tư", "Thứ Năm", "Thứ Sáu", "Thứ Bảy"];
+    
+        // Function to generate an array of the next 30 days with day of the week
+        const generateNext30Days = () => {
+          const today = new Date();
+          const daysArray = [];
+          for (let i = 0; i < 30; i++) {
+            const nextDay = new Date(today);
+            nextDay.setDate(today.getDate() + i + 1);
+            const dayOfWeek = daysOfWeek[nextDay.getDay()];
+    
+            // Format date to dd-MM-yyyy
+            const day = nextDay.getDate().toString().padStart(2, '0');
+            const month = (nextDay.getMonth() + 1).toString().padStart(2, '0');
+            const year = nextDay.getFullYear();
+            const formattedDate = `${day}-${month}-${year} (${dayOfWeek})`;
+            daysArray.push(formattedDate);
+          }
+          return daysArray;
+        };
+    
+        const next30Days = generateNext30Days();
+        setDanhSachNgayThucHien(next30Days);
+      }, []);
+
+
+      useEffect(() => {
+        const generateTimeRanges = (interval) => {
+          const timesArray = [];
+          for (let hour = 0; hour < 24; hour += interval) {
+            const startHour = hour.toString().padStart(2, '0');
+            const endHour = (hour + interval).toString().padStart(2, '0');
+            if (hour + interval <= 24) {
+              const timeString = `${startHour}:00 đến ${endHour}:00`;
+              timesArray.push(timeString);
+            }
+          }
+          return timesArray;
+        };
+    
+        const interval = donHangData.dichVuChinh.thoiGian || 1;
+        const timeRanges = generateTimeRanges(interval);
+        setDanhSachGioThucHien(timeRanges);
+      }, [donHangData.dichVuChinh.thoiGian]);
+
+
+      
     useEffect(() => {
         const fetchData = async () => {
             try {
@@ -56,20 +109,20 @@ const ThemDonHang = () => {
     useEffect(() => {
         const fetchData = async () => {
             try {
-               if(diaChiData.tinhTp?.code) {
-                const { data } = await apiQuanHuyen(diaChiData.tinhTp?.code);
-                setDanhSachQuanHuyen(data.DanhSachQuanHuyen)
-               }
+                if (diaChiData.tinhTp?.code) {
+                    const { data } = await apiQuanHuyen(diaChiData.tinhTp?.code);
+                    setDanhSachQuanHuyen(data.DanhSachQuanHuyen)
+                }
             } catch (error) {
                 console.error('Error fetching data:', error);
             }
         };
-        fetchData();    
+        fetchData();
     }, [diaChiData.tinhTp]);
     useEffect(() => {
         const fetchData = async () => {
             try {
-                if(diaChiData.quanHuyen?.code) {
+                if (diaChiData.quanHuyen?.code) {
                     const { data } = await apiXaPhuong(diaChiData.quanHuyen?.code);
                     setDanhSachXaPhuong(data.DanhSachXaPhuong)
                 }
@@ -100,15 +153,15 @@ const ThemDonHang = () => {
 
         setDiaChiData((prevData) => {
             const newData = { ...prevData, [name]: value };
-            
+
             // Reset các trường khác khi tinhTp thay đổi
             if (name === 'tinhTp') {
                 newData.quanHuyen = null;
                 newData.xaPhuong = null;
                 newData.soNhaTenDuong = '';
                 newData.ghiChu = '';
-                setDanhSachQuanHuyen ([]);
-                setDanhSachXaPhuong ([]);
+                setDanhSachQuanHuyen([]);
+                setDanhSachXaPhuong([]);
             }
             if (name === 'quanHuyen') {
                 newData.xaPhuong = null;
@@ -120,7 +173,7 @@ const ThemDonHang = () => {
                 newData.soNhaTenDuong = '';
                 newData.ghiChu = '';
             }
-            
+
             return newData;
         });
     };
@@ -153,7 +206,7 @@ const ThemDonHang = () => {
         e.preventDefault();
         // Thực hiện logic khi submit form
     };
-    
+
     return (
         <Paper sx={{ padding: '20px', marginTop: '15px' }}>
             <form onSubmit={handleSubmit}>
@@ -191,36 +244,36 @@ const ThemDonHang = () => {
                     </Grid>
                     <Grid item xs={6}>
                         <Typography><strong>Danh sách dich vụ thêm</strong></Typography>
-                    <Table>
-                <TableHead>
-                    <TableRow>
-                        <TableCell>Chọn</TableCell>
-                        <TableCell>Tên Dịch Vụ</TableCell>
-                        <TableCell>Giá</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {danhSachDichVuThem.map((dichVu) => (
-                        <TableRow key={dichVu.id} sx={{
-                            cursor: 'pointer',
-                            '&:hover': {
-                              backgroundColor: '#bec2cc',
-                            },
-                          }} onClick={() => handleSelect(dichVu.id)}>
-                            <TableCell>
-                                <Checkbox
-                                    checked={selectedDichVu.includes(dichVu.id)}
-                                    onChange={() => handleSelect(dichVu.id)}
-                                />
-                            </TableCell>
-                            <TableCell>{dichVu.tenDichVu}</TableCell>
-                            <TableCell>{dichVu.gia !== null ?  '+ '+ dichVu.gia + ' VNĐ' : '+ '+ dichVu.thoiGian + ' giờ'}</TableCell>
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
+                        <Table>
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>Chọn</TableCell>
+                                    <TableCell>Tên Dịch Vụ</TableCell>
+                                    <TableCell>Giá</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {danhSachDichVuThem.map((dichVu) => (
+                                    <TableRow key={dichVu.id} sx={{
+                                        cursor: 'pointer',
+                                        '&:hover': {
+                                            backgroundColor: '#bec2cc',
+                                        },
+                                    }} onClick={() => handleSelect(dichVu.id)}>
+                                        <TableCell>
+                                            <Checkbox
+                                                checked={selectedDichVu.includes(dichVu.id)}
+                                                onChange={() => handleSelect(dichVu.id)}
+                                            />
+                                        </TableCell>
+                                        <TableCell>{dichVu.tenDichVu}</TableCell>
+                                        <TableCell>{dichVu.gia !== null ? '+ ' + dichVu.gia + ' VNĐ' : '+ ' + dichVu.thoiGian + ' giờ'}</TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
                     </Grid>
-                  
+
                     <Grid item xs={6}>
                         <TextField
                             fullWidth
@@ -228,8 +281,43 @@ const ThemDonHang = () => {
                             name="ghiChu"
                             variant="outlined"
                             size="small"
+                            multiline
+                            minRows={6}
                             value={donHangData.ghiChu}
                             onChange={handleChangeDonHang}
+                        />
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Autocomplete
+                            options={danhSachNgayThucHien}
+                            value={donHangData.ngayBatDau}
+                            onChange={(event, newValue) => handleChangeDonHang({ target: { name: 'ngayBatDau', value: newValue } })}
+                            renderInput={(params) => (
+                                <TextField
+                                    {...params}
+                                    label="Chọn ngày thực hiện"
+                                    variant="outlined"
+                                    size="small"
+                                    fullWidth
+                                />
+                            )}
+                        />
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Autocomplete
+                            // getOptionLabel={(option) => option.tenDichVu}
+                            options={danhSachGioThucHien}
+                            // value={donHangData.dichVuChinh}
+                            // onChange={(event, newValue) => handleChangeDonHang({ target: { name: 'dichVuChinh', value: newValue } })}
+                            renderInput={(params) => (
+                                <TextField
+                                    {...params}
+                                    label="Chọn giờ bắt đầu và kết thúc"
+                                    variant="outlined"
+                                    size="small"
+                                    fullWidth
+                                />
+                            )}
                         />
                     </Grid>
                 </Grid>
@@ -295,7 +383,7 @@ const ThemDonHang = () => {
                         />
                     </Grid>
                     <Grid item xs={6}>
-                    <Autocomplete
+                        <Autocomplete
                             options={danhSachQuanHuyen}
                             getOptionLabel={(option) => option.name_with_type}
                             value={diaChiData.quanHuyen}
@@ -312,7 +400,7 @@ const ThemDonHang = () => {
                         />
                     </Grid>
                     <Grid item xs={6}>
-                    <Autocomplete
+                        <Autocomplete
                             options={danhSachXaPhuong}
                             getOptionLabel={(option) => option.name_with_type}
                             value={diaChiData.xaPhuong}
@@ -350,19 +438,6 @@ const ThemDonHang = () => {
                             onChange={handleChangeDiaChi}
                         />
                     </Grid>
-                    <Grid item xs={6}>
-                    <TextField
-                        fullWidth
-                        label="Vật nuôi"
-                        name="vatNuoi"
-                        variant="outlined"
-                        size="small"
-                        value={donHangData.vatNuoi}
-                        onChange={handleChangeDonHang}
-                        error={!!errors.vatNuoi}
-                        helperText={errors.vatNuoi}
-                    />
-                </Grid>
                 </Grid>
                 <br />
                 <Button type="submit" variant="contained" color="primary">Submit</Button>
