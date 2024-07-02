cc0e97ea206dac38d93fde527309393682774868
tuan23590 <thanhtuan.htcv@gmail.com>
2024-07-02 18:16:23 +0700

    feat: Add ChiTietDonHangModal component to NotifyScreen

--- client-app-nv/App/Screens/NotifyScreen/NotifyScreen.jsx
+++ client-app-nv/App/Screens/NotifyScreen/NotifyScreen.jsx
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, FlatList, StyleSheet } from 'react-native';
+import { View, Text, FlatList, StyleSheet, TouchableOpacity,ScrollView } from 'react-native';
 import GlobalAPI from '../../Utils/GlobalAPI';
 import { FIREBASE_AUTH } from '../../fireBase/config';
 import { onAuthStateChanged } from 'firebase/auth';
+import ChiTietDonHangModal from '../TrangChu/ChiTietDonHangModal';
 
 export default function NotifyScreen() {
   const [danhSachDonHang, setDanhSachDonHang] = useState([]);
+  const [selectedOrder, setSelectedOrder] = useState(null); 
+  const [modalVisible, setModalVisible] = useState(false); 
+
 
   const fetchData = async (user) => {
     const { TimNhanVienTheoEmail } = await GlobalAPI.apiNhanVienTheoEmail(user.email);
@@ -24,36 +28,37 @@ export default function NotifyScreen() {
   }, []);
 
   const renderItem = ({ item }) => {
-    let backgroundColor = '';
+    let trangThaiStyle = styles.default; // mặc định sử dụng style default
+
+    // Xác định style dựa vào trạng thái đơn hàng
     switch (item.trangThaiDonHang) {
-      case 'Đang chờ duyệt':
       case 'Đang thực hiện':
-        backgroundColor = '#a5d6a7';
-        break;
-      case 'Nhân viên từ chối':
-      case 'Đã từ chối':
-        backgroundColor = '#ef9a9a';
-        break;
-      case 'Chờ xác nhận':
-        backgroundColor = '#ffecb3';
+        trangThaiStyle = styles.dangThucHien;
         break;
       case 'Đã hoàn thành':
-        backgroundColor = '#b3e5fc';
-        break;
+        trangThaiStyle = styles.daHoanThanh;
+      break;
       default:
-        backgroundColor = '#ffffff';
+        trangThaiStyle = styles.default;
         break;
     }
-
+    const handlePress = async (item) => {
+      const {DonHangTheoId} = await GlobalAPI.apiChiTietDonHang(item.id);
+      setSelectedOrder(DonHangTheoId);
+      console.log(DonHangTheoId.khachHang);
+      setModalVisible(true);
+    }
     return (
-      <View style={[styles.item, { backgroundColor }]}>
+      <TouchableOpacity onPress={()=>handlePress(item)}>
+        <View style={[styles.item, { backgroundColor: '#e0e0e0' }]}>
         <Text style={{ fontWeight: 'bold', fontSize: 16 }}>Mã đơn hàng: {item.maDonHang}</Text>
         <Text>Ngày đặt hàng: {new Date(item.ngayDatHang).toLocaleDateString()}</Text>
         <Text>Khách hàng: {item.khachHang.tenKhachHang}</Text>
         <Text>Địa chỉ: {item.diaChi.soNhaTenDuong}, {item.diaChi.quanHuyen}, {item.diaChi.tinhTP}</Text>
         <Text>Tổng tiền: {item.tongTien} VNĐ</Text>
-        <Text>Trạng thái: {item.trangThaiDonHang}</Text>
+        <Text style={trangThaiStyle}>{`Trạng thái: ${item.trangThaiDonHang}`}</Text>
       </View>
+      </TouchableOpacity>
     );
   };
 
@@ -66,6 +71,14 @@ export default function NotifyScreen() {
         keyExtractor={(item) => item.id}
         showsVerticalScrollIndicator={false}
       />
+      <ScrollView showsVerticalScrollIndicator={false}>
+            <ChiTietDonHangModal
+                visible={modalVisible}
+                closeModal={() => setModalVisible(false)}
+                order={selectedOrder}
+                fetchData={fetchData}
+            />
+            </ScrollView>
     </View>
   );
 }
@@ -75,5 +88,20 @@ const styles = StyleSheet.create({
     padding: 20,
     marginVertical: 8,
     borderRadius: 10,
+  },
+  default: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#000000', // màu mặc định
+  },
+  dangThucHien: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#4caf50', // màu cho Đang thực hiện và Đang chờ duyệt
+  },
+  daHoanThanh: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#03a9f4', // màu cho Đã hoàn thành
   },
 });
