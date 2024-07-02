3c326438ac9077c05852bbcdb8694c5907f366ab
tuan23590 <thanhtuan.htcv@gmail.com>
2024-07-02 09:25:45 +0700

    Update GraphQL server URL to use production environment

--- client-web/utils/constants.js
+++ client-web/utils/constants.js
@@ -1,2 +1,7 @@
-export const GRAPHQL_SERVER = 'http://localhost:4000/';
+// export const GRAPHQL_SERVER = 'http://localhost:4000/';
+// export const IMAGE_SERVER = 'http://localhost:3000';
+
+
+
+export const GRAPHQL_SERVER = 'https://r0bw976s-4000.asse.devtunnels.ms/';
 export const IMAGE_SERVER = 'http://localhost:3000';
